68b97682f1cdf9c7710b156f110c6441300fea4d
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-07-27 09:18:28 +0200

    Activity Tracker project by Spring Boot & JPA

---
+++ activity-tracker-sb-jpa-solution/src/main/java/activities/ActivityTrackerSbJpaSolutionApplication.java
@@ -0,0 +1,12 @@
+package activities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTrackerSbJpaSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityTrackerSbJpaSolutionApplication.class, args);
+    }
+}
---
+++ activity-tracker-sb-jpa-solution/src/test/java/activities/ActivityTrackerSbJpaSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package activities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityTrackerSbJpaSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
