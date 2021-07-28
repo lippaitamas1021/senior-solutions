90351029b49f75ee85e07d0c6d3c212cda2e0453
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-07-28 14:33:07 +0200

    JDBC

--- meetingrooms-jdbc-solution/src/main/java/meetingroom/MeetingRoom.java
+++ meetingrooms-jdbc-solution/src/main/java/meetingroom/MeetingRoom.java
@@ -2,10 +2,8 @@ package meetingroom;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
